72c36c0993cb0310d98e358abdcfe9e88a28a20f
AxelPc <ericklkv7@gmail.com>
2024-03-23 15:32:14 -0600

    botones casibonitos

--- index.py
+++ index.py
@@ -19,11 +19,12 @@ class View(tk.Tk): ##clase vista y heredamos tk
         super().__init__()
         self.controller=controller ##acceder a la clase controller
         self.mi_frame12()
-        self.mi_frame4()
+        self.framegrid()
+        self.mi_frame3()
         self.mi_frame()
         self.mi_frame1()
         self.mi_frame2()
-        self.framedrid()
+        #self.framedrid()
         self.value_Var=tk.StringVar()
         self.title('Mi calculadora_hola') ##titulo de la ventana
         #self.titulo_etiqueta()
@@ -33,10 +34,8 @@ class View(tk.Tk): ##clase vista y heredamos tk
         self.boton_3a7()
         self.boton_0a4()
         self.simbolos()
-        self.boton_dividir()
-        self.mas()
-        self.menos()
-        #self.boton_grid2()
+        #self.boton_arriba()
+        
         
     
         
@@ -57,11 +56,11 @@ class View(tk.Tk): ##clase vista y heredamos tk
         self.main_frame2=ttk.Frame(self)##creamos el marco o ventana para que salte 
         self.main_frame2.pack()#padx=30, pady=30
 
-    def mi_frame4(self):
+    def mi_frame3(self):
         self.main_frame3=ttk.Frame(self)# height=200, width=100)##creamos el marco o ventana para que salte 
         self.main_frame3.pack()#padx=30, pady=30)
 
-    def framedrid(self):
+    def framegrid(self):
         self.frame_grid=ttk.Frame(self)# height=200, width=100)##creamos el marco o ventana para que salte 
         self.frame_grid.pack()#padx=30, pady=30)
 
@@ -75,45 +74,52 @@ class View(tk.Tk): ##clase vista y heredamos tk
         self.la=ttk.Label(self.main_frame, text='Calculadora Hisoka', font=('Arial', 18))
         self.la.pack()#padx=30, pady=50)
     
-    number=['1','2','3','4','5','6','7','8','9','/']
-    simbols=['0','X', '+','/','C','-','=']
+    number=['1','2','3','4','5','6','7','8','9','+','/','-']
+    simbols=['0','X', '+','/','.','-','%','C','=']
     
-    ##botones de numero
+    ##botones de numero 4 al 7 y /
     def boton_3a7(self):
         for numero in self.number:
-             if numero > '3' and numero < '7':   
+            if numero > '3' and numero < '7':   
                 oneButton = tk.Button(self.main_frame, text=numero,width=5, padx=10, pady=10, font=("Arial", 20),command=lambda num=numero: self.controller.click_boton(num))
                 oneButton.pack(side='left')#, padx=5,pady=5)
+            elif numero =='/':
+                oneButton1 = tk.Button(self.main_frame, text=numero,width=5, padx=10, pady=10, font=("Arial", 20),command=lambda num=numero: self.controller.click_boton(num), bg='lightblue')#, bg='blue')
+                oneButton1.pack(side='left')
     
-    def boton_dividir(self):
-        for numero in self.simbols:
-            if numero == '/':
-                oneButton = tk.Button(self.main_frame, text=numero,width=5, padx=10, pady=10, font=("Arial", 20), bg='lightblue')
-                oneButton.pack(side='left')#, padx=5,pady=5)
-
-
-
-
+    #numero 1 al 3 y +
     def boton_0a4(self):
         for numero in self.number:
             if numero > '0' and numero < '4':
                 oneButton1 = tk.Button(self.main_frame1, text=numero,width=5, padx=10, pady=10, font=("Arial", 20), command=lambda num=numero: self.controller.click_boton(num))
                 oneButton1.pack(side='left')#, padx=10,pady=10)
+            elif numero =='+':
+                oneButton1 = tk.Button(self.main_frame1, text=numero,width=5, padx=10, pady=10, font=("Arial", 20),command=lambda num=numero: self.controller.click_boton(num), bg='lightblue')#, bg='blue')
+                oneButton1.pack(side='left')
     
-    def mas(self):
-        for numero in self.simbols:
-            if numero == '+':
-                oneButton = tk.Button(self.main_frame1, text=numero,width=5, padx=10, pady=10, font=("Arial", 20), bg='lightblue')
-                oneButton.pack(side='left')#, padx=5,pady=5)
 
-    
+     
+    #simbolos 
     def simbolos(self):
         for numero in self.simbols:
-            if numero == 't' or numero == 'X' or numero == 'C' or numero =='=':    
+            if numero == '.':    
                 oneButton1 = tk.Button(self.main_frame2, text=numero,width=5, padx=10, pady=10, font=("Arial", 20),command=lambda num=numero: self.controller.click_boton(num), bg='lightblue')#, bg='blue')
-                oneButton1.pack(side='left')#,ipadx=20,ipady=30, anchor=tk.E)# padx=10,pady=10)
+                oneButton1.pack(side='left')
+
+            elif numero =='=':
+                oneButton1 = tk.Button(self.main_frame2, text=numero,width=11, padx=10, pady=10, font=("Arial", 20),command=lambda num=numero: self.controller.click_boton(num), bg='pink')
+                oneButton1.pack(side='left')
+
             elif numero =='0':
-                oneButton1 = tk.Button(self.main_frame2, text=numero,width=5, padx=10, pady=10, font=("Arial", 20),command=lambda num=numero: self.controller.click_boton(num))#, bg='blue')
+                oneButton1 = tk.Button(self.main_frame2, text=numero,width=6, padx=10, pady=10, font=("Arial", 20),command=lambda num=numero: self.controller.click_boton(num))#, bg='blue')
+                oneButton1.pack(side='left')
+
+            elif numero =='X' or numero =='%':
+                oneButton1 = tk.Button(self.frame_grid, text=numero,width=5, padx=10, pady=10, font=("Arial", 20),command=lambda num=numero: self.controller.click_boton(num), bg='lightblue')
+                oneButton1.pack(side='left')
+            
+            elif numero =='C':
+                oneButton1 = tk.Button(self.frame_grid, text=numero,width=10, padx=10, pady=10, font=("Arial", 20),command=lambda num=numero: self.controller.click_boton(num), bg='orange')
                 oneButton1.pack(side='left')
     
 
@@ -123,23 +129,21 @@ class View(tk.Tk): ##clase vista y heredamos tk
             if numero > '6' and numero <= '9':    
                 oneButton1 = tk.Button(self.main_frame3, text=numero,width=5, padx=10, pady=10, font=("Arial", 20), command=lambda num=numero: self.controller.click_boton(num))
                 oneButton1.pack(side='left')#,ipadx=20,ipady=30, anchor=tk.E)# padx=10,pady=10)
+            elif numero =='-':
+                oneButton1 = tk.Button(self.main_frame3, text=numero,width=5, padx=10, pady=10, font=("Arial", 20),command=lambda num=numero: self.controller.click_boton(num), bg='lightblue')
+                oneButton1.pack(side='left')
 
-    def menos(self):
-        for numero in self.simbols:
-            if numero == '-':
-                oneButton = tk.Button(self.main_frame3, text=numero,width=5, padx=10, pady=10, font=("Arial", 20), bg='lightblue')
-                oneButton.pack(side='left')#, padx=5,pady=5)
 
-    def boton_grid2(self):
-        tk.Button(self.frame_grid,activebackground='#73f5d7', bg="#f5f0f0", text=".", padx=22, pady=15).grid(row=6, column=2)# command=lambda num='.': self.controller.click_boton('')).grid(row=6, column=2)
-        tk.Button(self.frame_grid,activebackground='#73f5d7', bg="#f5f0f0", text="0", padx=50, pady=15,command=lambda num='0': self.controller.click_boton(num)).grid(row=6, column=0)#, columnspan=2) indica cuantas columnas ocupa
-        tk.Button(self.frame_grid,activebackground='#73f5d7', bg="#f5f0f0", text="C", padx=50, pady=15 ).grid(row=6, column=3)#, columnspan=2)
-        tk.Button(self.frame_grid,activebackground='#73f5d7', bg="#f5f0f0", text="=", padx=50, pady=15 ).grid(row=6, column=4)#), columnspan=2)
-            
-    
+
     def main(self):###función para lanzar la ventana
         self.mainloop()
 
+#def boton_arriba(self):
+     #   tk.Button(self.frame_grid,activebackground='#73f5d7', bg="#f5f0f0", text="%", padx=50, pady=15).grid(row=6, column=1)# command=lambda num='.': self.controller.click_boton('')).grid(row=6, column=2)
+      #  #tk.Button(self.frame_grid,activebackground='#73f5d7', bg="#f5f0f0", text="0", padx=50, pady=15,command=lambda num='0': self.controller.click_boton(num)).grid(row=6, column=0)#, columnspan=2) indica cuantas columnas ocupa
+       # tk.Button(self.frame_grid,activebackground='#73f5d7', bg="#f5f0f0", text="x", padx=50, pady=15 ).grid(row=6, column=3)#, columnspan=1)
+        #tk.Button(self.frame_grid,activebackground='#73f5d7', bg="#f5f0f0", text="=", padx=50, pady=15 ).grid(row=6, column=4, rowspan=2)
+          
 ##tengo dos opciones una usar el pack pero no se acomodarán como me gusta o la otra es usar el grid
     #otra opcion es aplicar if y elif, en los if irían los nunmero sy en los elif los imbolos
 
@@ -151,7 +155,8 @@ class Model: ##clase modelo
         if numero =='C':
             self.value=''
         #else: pass
-        
+        elif numero=='+':
+            self.value='gg'
         else:
             self.value += str(numero)    
         #elif numero == str:
